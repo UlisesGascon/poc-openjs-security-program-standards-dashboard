9da90abc4d9b164699f16ac328f11296eb936d9c
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2024-11-19 02:33:24 +0100

    chore: lint files

--- schemas/project.js
+++ schemas/project.js
@@ -1,42 +1,39 @@
-import Ajv from "ajv"
-import addFormats from "ajv-formats"
+import Ajv from 'ajv'
+import addFormats from 'ajv-formats'
 
 const ajv = new Ajv()
 addFormats(ajv)
 
 const schema = {
-    type: "object",
-    properties: {
-      name: { type: "string" },
-      githubOrgs: {
-        type: "array",
-        items: {
-          type: "object",
-          properties: {
-            url: { type: "string", format: "uri" },
-            name: { type: "string" }
-          },
-          required: ["url", "name"],
-          additionalProperties: false
-        }
+  type: 'object',
+  properties: {
+    name: { type: 'string' },
+    githubOrgs: {
+      type: 'array',
+      items: {
+        type: 'object',
+        properties: {
+          url: { type: 'string', format: 'uri' },
+          name: { type: 'string' }
+        },
+        required: ['url', 'name'],
+        additionalProperties: false
       }
-    },
-    required: ["name", "githubOrgs"],
-    additionalProperties: false
-  }
-
-
-
-  const validate = ajv.compile(schema)
-
-
-export function validateProject(project) {
-    const valid = validate(project)
-    if (!valid) {
-      throw new Error(validate.errors.map((error) => error.message).join("\n"))
     }
+  },
+  required: ['name', 'githubOrgs'],
+  additionalProperties: false
 }
 
-export function validateAllProjects(projects) {
-    projects.forEach(validateProject)
+const validate = ajv.compile(schema)
+
+export function validateProject (project) {
+  const valid = validate(project)
+  if (!valid) {
+    throw new Error(validate.errors.map((error) => error.message).join('\n'))
   }
+}
+
+export function validateAllProjects (projects) {
+  projects.forEach(validateProject)
+}
