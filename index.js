a588b40c6cfde7b6dea79efa3d284801e6929bad
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2024-11-19 01:44:55 +0100

    refactor: remove demo references

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 
-import { runOrderCommand } from './lib/cli.js'
 import { Command } from 'commander'
+import { runAddProjectCommand } from './lib/cli/project.js'
 
 const program = new Command()
 
 program
-  .command('order <item>')
-  .description('Order a specific item')
-  .option('--size <size>', 'Size of the pizza')
-  .option('--cheese <cheese>', 'Number of cheese layers', parseInt)
-  .action((item, options) => {
-    runOrderCommand(item, options)
+  .command('project add')
+  .description('Add a new project')
+  .option('--name <name>', 'Name of the project')
+  .option('--github-urls <urls...>', 'GitHub URLs of the project')
+  .action((options) => {
+    runAddProjectCommand(options)
   })
 
 program.parse(process.argv)
