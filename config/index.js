e462c93c280f164bfdef0e97f6408864d543c81b
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2024-11-19 16:16:27 +0100

    feat: extend configuration to filter relevant repositories properties

--- config/index.js
+++ config/index.js
@@ -1,11 +1,15 @@
 import { join } from 'path'
 
+const relevantRepoProperties = ['id', 'node_id', 'name', 'full_name', 'private', 'html_url', 'description', 'fork', 'created_at', 'updated_at', 'pushed_at', 'size', 'stargazers_count', 'watchers_count', 'language', 'has_issues', 'has_projects', 'has_downloads', 'has_wiki', 'has_pages', 'has_discussions', 'forks_count', 'mirror_url', 'archived', 'disabled', 'open_issues_count', 'license', 'allow_forking', 'is_template', 'web_commit_signoff_required', 'topics', 'visibility', 'forks', 'open_issues', 'watchers', 'default_branch', 'permissions']
+
 const defaultValues = {
-  dataPath: join(process.cwd(), 'data')
+  dataPath: join(process.cwd(), 'data'),
+  relevantRepoProperties
 }
 
 const testEnvironment = {
-  dataPath: join(process.cwd(), '__tests__', 'data')
+  dataPath: join(process.cwd(), '__tests__', 'data'),
+  relevantRepoProperties
 }
 
 export function getConfig (env) {
