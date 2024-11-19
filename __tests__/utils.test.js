d574d654829d5c922b12c733ac86aaa933938709
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2024-11-19 16:15:59 +0100

    test: make tests more resilient

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,6 +1,16 @@
 import { ensureGithubToken } from '../lib/utils/index.js'
 
 describe('ensureGithubToken', () => {
+  let originalGithubToken
+
+  beforeAll(() => {
+    originalGithubToken = process.env.GITHUB_TOKEN
+  })
+
+  afterAll(() => {
+    process.env.GITHUB_TOKEN = originalGithubToken
+  })
+
   it('should throw an error if GITHUB_TOKEN is required', () => {
     delete process.env.GITHUB_TOKEN
     expect(() => ensureGithubToken()).toThrow('GITHUB_TOKEN is required')
