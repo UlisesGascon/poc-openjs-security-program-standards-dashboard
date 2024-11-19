d1e3572ae4376718da5a62fed95660e10afda69a
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2024-11-19 12:06:30 +0100

    feat: add environment configuration

--- __tests__/integrity.test.js
+++ __tests__/integrity.test.js
@@ -1,10 +1,15 @@
 import { test } from 'node:test'
 import assert from 'node:assert'
 import fs from 'node:fs'
+import { join } from 'node:path'
 
 import { validateAllProjects } from '../schemas/project.js'
+import { getConfig } from '../config/index.js'
+
+// Ensure that we use the production configuration
+const { dataPath } = getConfig('production')
 
 test('Validate all projects data', () => {
-  const projects = JSON.parse(fs.readFileSync('data/projects.json', 'utf-8'))
+  const projects = JSON.parse(fs.readFileSync(join(dataPath, 'projects.json'), 'utf-8'))
   assert.doesNotThrow(() => validateAllProjects(projects), 'Expected all projects to be valid')
 })
