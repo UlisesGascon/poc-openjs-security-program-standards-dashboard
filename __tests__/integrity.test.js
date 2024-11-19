a39247f9cbe0cf43e6257f5dea0f1bed2a23690b
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2024-11-19 02:33:24 +0100

    chore: lint files

--- __tests__/integrity.test.js
+++ __tests__/integrity.test.js
@@ -1,10 +1,10 @@
-import { test } from 'node:test';
-import assert from 'node:assert';
-import fs from 'node:fs';
+import { test } from 'node:test'
+import assert from 'node:assert'
+import fs from 'node:fs'
 
-import { validateAllProjects } from '../schemas/project.js';
+import { validateAllProjects } from '../schemas/project.js'
 
 test('Validate all projects data', () => {
-  const projects = JSON.parse(fs.readFileSync('data/projects.json', 'utf-8'));
-  assert.doesNotThrow(() => validateAllProjects(projects), 'Expected all projects to be valid');
+  const projects = JSON.parse(fs.readFileSync('data/projects.json', 'utf-8'))
+  assert.doesNotThrow(() => validateAllProjects(projects), 'Expected all projects to be valid')
 })
