2c65a61c2a79cc5fffc59823bc55419e3f03d903
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2024-11-19 16:15:35 +0100

    test: improve configuration

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,11 @@
 export default {
+  collectCoverageFrom: [
+    'lib/**/*.js'
+  ],
   coveragePathIgnorePatterns: [
     '<rootDir>/__tests__/'
+  ],
+  transformIgnorePatterns: [
+    '/node_modules/(?!octokit).+\\.js$'
   ]
 }
