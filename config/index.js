680ddcbffd5fbf396b30edeb315da34ee65e291d
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2024-11-20 10:26:43 +0100

    feat: add category to checks
    Signed-off-by: Ulises Gascon <ulisesgascongonzalez@gmail.com>

--- config/index.js
+++ config/index.js
@@ -2,17 +2,31 @@ import { join } from 'path'
 
 const relevantRepoProperties = ['id', 'node_id', 'name', 'full_name', 'private', 'html_url', 'description', 'fork', 'created_at', 'updated_at', 'pushed_at', 'size', 'stargazers_count', 'watchers_count', 'language', 'has_issues', 'has_projects', 'has_downloads', 'has_wiki', 'has_pages', 'has_discussions', 'forks_count', 'mirror_url', 'archived', 'disabled', 'open_issues_count', 'license', 'allow_forking', 'is_template', 'web_commit_signoff_required', 'topics', 'visibility', 'forks', 'open_issues', 'watchers', 'default_branch', 'permissions']
 const projectCategories = ['impact', 'at-large', 'incubation', 'emeritus']
+const checksCategories = [
+  'user authentication',
+  'user account permissions',
+  'service authentication',
+  'github workflow permissions',
+  'vulnerability management',
+  'coordinated vulnerability disclosure',
+  'code quality',
+  'code review',
+  'source control',
+  'dependency inventory'
+]
 
 const defaultValues = {
   dataPath: join(process.cwd(), 'data'),
   relevantRepoProperties,
-  projectCategories
+  projectCategories,
+  checksCategories
 }
 
 const testEnvironment = {
   dataPath: join(process.cwd(), '__tests__', 'data'),
   relevantRepoProperties,
-  projectCategories
+  projectCategories,
+  checksCategories
 }
 
 export function getConfig (env) {
