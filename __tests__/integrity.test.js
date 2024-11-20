74fd22b78ee12016e921cf71ceb733baa0948c5e
Ulises Gascon <ulisesgascongonzalez@gmail.com>
2024-11-20 10:06:46 +0100

    test: add validation for checks

--- __tests__/integrity.test.js
+++ __tests__/integrity.test.js
@@ -1,6 +1,7 @@
 import fs from 'fs'
 import { join } from 'path'
 import { validateAllProjects } from '../schemas/project.js'
+import { validateAllChecks } from '../schemas/check.js'
 import { getConfig } from '../config/index.js'
 
 const { dataPath } = getConfig('default')
@@ -8,4 +9,9 @@ const { dataPath } = getConfig('default')
 test('Validate all projects data', () => {
   const projects = JSON.parse(fs.readFileSync(join(dataPath, 'projects.json'), 'utf-8'))
   expect(() => validateAllProjects(projects)).not.toThrow()
+})
+
+test('Validate all checks data', () => {
+  const checks = JSON.parse(fs.readFileSync(join(dataPath, 'checks.json'), 'utf-8'))
+  expect(() => validateAllChecks(checks)).not.toThrow()
 })
